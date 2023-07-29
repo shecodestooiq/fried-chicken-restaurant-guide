764156bdf2d52cb7ad1ce38b00afc4dd9d735d0f
AthraaMosawi <athraamosawi169@gmail.com>
2023-07-29 04:28:44 +0300

    create fried chicken restaurants guide

--- src/App.js
+++ src/App.js
@@ -1,7 +1,13 @@
+import React from 'react';
 import './App.css';
+import RestaurantsList from './components/RestaurantsList';
 
 function App() {
-  return <>Fried Chicken Guide Web App</>;
+  return( 
+  <>
+    <RestaurantsList />
+  </>
+  );
 }
 
 export default App;
---
+++ src/components/RestaurantsList.jsx
@@ -0,0 +1,98 @@
+import React, { useState } from 'react';
+import img1 from '../Food_Logo.png'
+
+export default function RestaurantsList() {
+    const [restaurant, setRestaurant] = useState([{
+        id:1,
+        name: "FireFly",
+        location: "Yarmouk",
+        range: 90
+    }, {
+        id:2,
+        name: "Foodland",
+        location: "Harthyia",
+        range: 30
+    }, {
+        id:3,
+        name: "Yumyum",
+        location: "Saydia",
+        range: 80
+    }]);
+    
+    const [nameValue, setNameValue] = useState('');
+    const [locationValue, setLocationValue] = useState('');
+    const [rangeValue, setRangeValue] = useState('');
+
+    const nameChange = (event) => {
+        setNameValue(event.target.value)
+    };
+    const locationChange = (event) => {
+        setLocationValue(event.target.value)
+    };
+    const rangeChange = (event) => {
+        setRangeValue(event.target.value)
+    };
+
+    const addRestaurant = () => {
+        setRestaurant(previousInputs => ([...previousInputs,{id: restaurant.length + 1,name: nameValue, location: locationValue, range: rangeValue}]));
+        setNameValue("")
+        setLocationValue("")
+        setRangeValue("")
+    }
+
+    const deleteRestaurant = (id) => {
+        setRestaurant(previousInputs => {
+            return previousInputs.filter(rest =>
+            rest.id !== id)
+        })
+    };
+
+    const updateRange = (id) => {
+        setRestaurant(previousInputs => {
+            return previousInputs.map(rest => {
+                if (rest.id === id) {
+                    return {...rest, range: rangeValue};
+                }
+                return rest
+            })
+        })
+    }
+
+    return(
+        <>
+            <div className='header'>
+                <div>
+                    <h1>Explore The Highest Rating Restaurants For Fried Chicken!</h1>
+                    <h2>Start Scrolling...</h2>
+                </div>
+            </div>
+            <div className='mainSection'>
+                <h1>Recommended Fried Chicken Restaurants in Baghdad!</h1>
+                <div className='grid'>
+                    <input type="text" value={nameValue} onChange={nameChange} placeholder="Restaurant name"/>
+                    <input type="text" value={locationValue} onChange={locationChange} placeholder="Restaurant location"/>
+                    <input type="range" value={rangeValue} onChange={rangeChange}/>
+                    <button className='AddButton' onClick={addRestaurant}>Add New Restaurant</button>
+                </div>
+                <ul className='restaurantsCards'>
+                    {restaurant.map((rest,index) => (
+                    <li key={index} className='card'>
+                        <img src={img1} alt="" />
+                        <h3>{rest.name}</h3>
+                        <h4>Location: {rest.location}</h4>
+                        <h4>Rate: {rest.range}</h4>
+                        <div className='cardButton'>
+                        <button onClick={()=>deleteRestaurant(rest.id)}>Delete</button>
+                        <button onClick={()=>updateRange(rest.id)}>Change Rate</button></div>
+                    </li>))}
+                </ul>
+            </div>
+            <div className='footer'>
+                <div>
+                    <h2>Have a good meal :)</h2>
+                </div>
+                {/* <div className='imgContainer'><img src={img} className="headerImg"/></div> */}
+            </div>
+        </>
+    );
+}
