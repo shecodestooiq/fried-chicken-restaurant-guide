91c68f729a131a9561fac9a1306cb26afbad2b55
AthraaMosawi <athraamosawi169@gmail.com>
2023-08-04 16:53:24 +0300

    make a separate components and add a link/route on each resturant card

--- src/components/RestaurantsList.jsx
+++ src/components/RestaurantsList.jsx
@@ -1,99 +1,74 @@
 import React, { useState } from 'react';
-import img1 from '../Food_Logo2.png'
+import RestaurantCard from './RestaurantCard';
 
-export default function RestaurantsList() {
-    const [restaurant, setRestaurant] = useState([{
-        id:1,
-        name: "FireFly",
-        location: "Yarmouk",
-        range: 90
-    }, {
-        id:2,
-        name: "Foodland",
-        location: "Harthyia",
-        range: 30
-    }, {
-        id:3,
-        name: "Yumyum",
-        location: "Saydia",
-        range: 80
-    }]);
-    
+export default function RestaurantsList({ restaurant, setRestaurant }) {
     const [nameValue, setNameValue] = useState('');
     const [locationValue, setLocationValue] = useState('');
     const [rangeValue, setRangeValue] = useState('');
 
     const nameChange = (event) => {
-        setNameValue(event.target.value)
+        setNameValue(event.target.value);
     };
+
     const locationChange = (event) => {
-        setLocationValue(event.target.value)
+        setLocationValue(event.target.value);
     };
+
     const rangeChange = (event) => {
-        setRangeValue(event.target.value)
+        setRangeValue(event.target.value);
     };
 
     const addRestaurant = () => {
-        setRestaurant(previousInputs => ([...previousInputs,{id: restaurant.length + 1,name: nameValue, location: locationValue, range: rangeValue}]));
-        setNameValue("")
-        setLocationValue("")
-        setRangeValue("")
-    }
+        setRestaurant((previousInputs) => [
+        ...previousInputs,
+        {
+            id: previousInputs.length + 1,
+            name: nameValue,
+            location: locationValue,
+            range: rangeValue,
+        },
+        ]);
+        setNameValue('');
+        setLocationValue('');
+        setRangeValue('');
+    };
 
     const deleteRestaurant = (id) => {
-        setRestaurant(previousInputs => {
-            return previousInputs.filter(rest =>
-            rest.id !== id)
-        })
+        setRestaurant((previousInputs) => {
+        return previousInputs.filter((rest) => rest.id !== id);
+        });
     };
 
     const updateRange = (id) => {
-        setRestaurant(previousInputs => {
-            return previousInputs.map(rest => {
-                if (rest.id === id) {
-                    return {...rest, range: rangeValue};
-                }
-                return rest
-            })
-        })
-    }
+        setRestaurant((previousInputs) => {
+        return previousInputs.map((rest) => {
+            if (rest.id === id) {
+            return { ...rest, range: rangeValue };
+            }
+            return rest;
+        });
+        });
+    };
 
-    return(
+    return (
         <>
-            <div className='header'>
-                <div>
-                    <h1>Discover The Highest Rated Restaurants For Fried Chicken!🍗 </h1>
-                    <h2>Start Scrolling...</h2>
-                </div>
-            </div>
-            <div className='mainSection'>
+            <div className='mainSection' id='addRest'>
                 <h1>Explore and Add!</h1>
                 <h2>Recommended Fried Chicken Restaurants in Baghdad with Special Deals!</h2>
                 <div className='grid'>
-                    <input type="text" value={nameValue} onChange={nameChange} placeholder="Restaurant name"/>
-                    <input type="text" value={locationValue} onChange={locationChange} placeholder="Restaurant location"/>
-                    <label htmlFor="range">Rate the restaurant from 1 to 100:</label>
-                    <input type="range" value={rangeValue} onChange={rangeChange}/>
-                    <button className='AddButton' onClick={addRestaurant}>Add New Restaurant</button>
+                <input type='text' value={nameValue} onChange={nameChange} placeholder='Restaurant name'/>
+                <input type='text' value={locationValue} onChange={locationChange} placeholder='Restaurant location' />
+                <label htmlFor='range'>Rate the restaurant from 1 to 100:</label>
+                <input type='range' value={rangeValue} onChange={rangeChange} />
+                <button className='AddButton' onClick={addRestaurant}>
+                    Add New Restaurant
+                </button>
                 </div>
-                <ul className='restaurantsCards'>
-                    {restaurant.map((rest,index) => (
-                    <li key={index} className='card'>
-                        <img src={img1} alt="" />
-                        <h3>{rest.name}</h3>
-                        <h4>Location: {rest.location}</h4>
-                        <h4 className='rateColor'>Rate: ⭐️⭐️⭐️⭐️ {rest.range}</h4>
-                        <div className='cardButton'>
-                        <button onClick={()=>deleteRestaurant(rest.id)}>Delete</button>
-                        <button onClick={()=>updateRange(rest.id)}>Change Rate</button></div>
-                    </li>))}
-                </ul>
-            </div>
-            <div className='footer'>
-                <div>
-                    <h2>Have a good meal :) ❤️</h2>
-                </div>
-                {/* <div className='imgContainer'><img src={img} className="headerImg"/></div> */}
+                <RestaurantCard
+                    restaurant={restaurant}
+                    deleteRestaurant={deleteRestaurant}
+                    updateRange={updateRange}
+                />
             </div>
         </>
     );
